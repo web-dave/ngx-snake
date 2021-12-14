0e40c8d4e6a4e6b9834524d881c7994dadfd5fcb
web-dave <dave@webdave.de>
2021-12-14 21:16:42 +0100

    inital project

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'snake-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
