fee09f44ead3072b069b3b0b5313809b50c6b207
A. Sauder <info@netzwerkcenter.ch>
2021-12-21 18:14:16 +0100

    add prisma and docker-compose template for local development db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SystemController } from './features/system/system.controller';
 import {ConfigModule} from "@nestjs/config";
+import {PrismaService} from "./prisma.service";
 
 @Module({
   imports: [
     ConfigModule.forRoot()
   ],
   controllers: [SystemController],
-  providers: [],
+  providers: [
+    PrismaService
+  ],
 })
 export class AppModule {}
