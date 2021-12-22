c2a6b3bab3318d1159a4cbdabb99086cccb99280
A. Sauder <andre.sauder@netzwerkcenter.ch>
2021-12-22 18:43:51 +0100

    remove level filtering parameter

--- backend/src/features/hall-of-fame/hall-of-fame.controller.ts
+++ backend/src/features/hall-of-fame/hall-of-fame.controller.ts
@@ -1,16 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HallOfFameService } from './hall-of-fame.service';
 import { ScoreEntryDto } from './model/score.entry.dto';
-import { ScoreEntryLevel } from '@prisma/client';
 
 @Controller('api/v1/hall-of-fame')
 export class HallOfFameController {
   constructor(private service: HallOfFameService) {}
 
-  @Get('/:level')
-  async getList(
-    @Param('level') level: ScoreEntryLevel,
-  ): Promise<ScoreEntryDto[]> {
-    return this.service.getList(level);
+  @Get()
+  async getList(): Promise<ScoreEntryDto[]> {
+    return this.service.getList();
   }
 }
--- backend/src/features/hall-of-fame/hall-of-fame.service.ts
+++ backend/src/features/hall-of-fame/hall-of-fame.service.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
 import { ScoreEntryDto } from './model/score.entry.dto';
-import { ScoreEntryLevel } from '@prisma/client';
 
 @Injectable()
 export class HallOfFameService {
   constructor(private prisma: PrismaService) {}
 
-  async getList(level: ScoreEntryLevel): Promise<ScoreEntryDto[]> {
-    const qr = await this.prisma.scoreEntry.findMany({
-      where: {
-        level: level,
-      },
-    });
+  async getList(): Promise<ScoreEntryDto[]> {
+    const qr = await this.prisma.scoreEntry.findMany({});
     return qr.map((scoreEntry) => {
       return {
         id: scoreEntry.id,
