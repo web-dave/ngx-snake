af5f377cc38a91c8bed4286707f6f30c096e329b
A. Sauder <andre.sauder@netzwerkcenter.ch>
2021-12-22 19:09:34 +0100

    add openApi infos

--- backend/src/features/system/system.controller.ts
+++ backend/src/features/system/system.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { PingResponse } from './model/ping.response';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('/api/v1')
 export class SystemController {
-
   @Get('/ping')
+  @ApiResponse({
+    status: 200,
+    type: null,
+    description: 'check endpoint for be if running',
+  })
   async ping(): Promise<PingResponse> {
-    return Promise.resolve({ timestamp: Date.now() });
+    return Promise.resolve({ pong: Date.now() });
   }
 }
