80345330c5e13aeb3920b8ef38ecdf57e686c050
A. Sauder <andre.sauder@netzwerkcenter.ch>
2022-01-11 07:28:12 +0100

    activate cors config with no access limit on domain

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const logger: Logger = new Logger('bootstrap');
 
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
 
   const configService: ConfigService = app.get<ConfigService>(ConfigService);
 
